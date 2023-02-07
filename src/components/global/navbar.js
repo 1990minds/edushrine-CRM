9ad221a23a73a1d9893c38d15880535b08c4ae29
1990minds <1990minds@gmail.com>
2023-02-07 11:18:39 +0530

    update

--- src/components/global/navbar.js
+++ src/components/global/navbar.js
@@ -5,7 +5,7 @@ import {CgProfile} from 'react-icons/cg'
 import {Link} from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { logOut } from '../../api/authSlice'
-
+import {Select} from '@chakra-ui/react'
 
 export default function Navbar() {
   const [dropevent, setDropevent] = useState(false)
@@ -19,29 +19,31 @@ export default function Navbar() {
   return (
     <div class="sticky top-0  z-50">
 <section class="relative">
-  <nav class="flex justify-between py-8 bg-white mb-3">
+  <nav class="flex justify-between bg-white mb-3">
     <div class="w-full xl:w-auto px-2">
       <div class="flex items-center justify-between">
-        <a class="inline-flex items-center h-6" href="/">
-          <img src={Edulogo} alt=""/>
+        <a class="inline-flex items-center" href="/">
+          <img class="h-16" src={Edulogo} alt=""/>
         </a>
         
       </div>
     </div>
-    <div>
+    
+
+    <div class="mt-5">
   
     <div className=' text-red-600 text-3xl mr-10'> <CgProfile onClick={()=>setDropevent(!dropevent)}/></div>
-  
 
   {dropevent && <div className="dropdown_menu">
   <ul class="mt-2 font-bold">
-  {/* <li><Link to='/' >Profile</Link></li> */}
 
 <li className="flex text-gray-800  text-md rounded-lg  shrink-0 " onClick={logOutHandler} >Logout</li>
 
   </ul>
   </div>}
   </div>
+
+
 
       </nav>
   {/* -------------------------------------------------- */}
